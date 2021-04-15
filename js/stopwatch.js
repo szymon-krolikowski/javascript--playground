6a04cce5e19eed2ec353580b41f9ad8b3956a61c
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-04-15 17:59:03 +0200

    ADD clearCountDown function

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,5 +1,6 @@
 const startButton = document.querySelector(".buttonStart");
 const stopButton = document.querySelector(".buttonStop");
+const clearButton = document.querySelector(".buttonClear");
 const time = document.querySelector(".time");
 
 let countTime;
@@ -15,40 +16,52 @@ const startCountDown = () => {
     clearInterval(countTime)
     countTime = setInterval(() => {
       if(millisecondsUnity < 9) {
-          millisecondsUnity++
-          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+          millisecondsUnity++;
+          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } else if(millisecondsUnity === 9) {
-        millisecondsUnity = 0
-        millisecondsDecimal++
-        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+        millisecondsUnity = 0;
+        millisecondsDecimal++;
+        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } if(millisecondsDecimal === 10) {
         millisecondsUnity = 0;
         millisecondsDecimal = 0;
-        secoundsUnity++
-        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+        secoundsUnity++;
+        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } else if(millisecondsDecimal === 10 && secoundsUnity < 9) {
-        secoundsUnity++
-        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+        secoundsUnity++;
+        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } if(secoundsUnity === 10) {
-        secoundsUnity = 0
-        secoundsDecimal++
-        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+        secoundsUnity = 0;
+        secoundsDecimal++;
+        time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } if(secoundsDecimal === 6 && secoundsUnity < 10) {
-          minutesUnity++
-          secoundsUnity = 0
-          secoundsDecimal = 0
-          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
+          minutesUnity++;
+          secoundsUnity = 0;
+          secoundsDecimal = 0;
+          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
       } if(minutesUnity > 1 && minutesUnity === 10) {
-          minutesUnity = 0
-          minutesDecimal++
-          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`
-      }
-    }, 10)
-}
+          minutesUnity = 0;
+          minutesDecimal++;
+          time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
+      };
+    }, 10);
+};
 
 const stopCountDown = () => {
-  clearInterval(countTime)
-}
+  clearInterval(countTime);
+};
 
-startButton.addEventListener("click", startCountDown)
-stopButton.addEventListener("click", stopCountDown)
+const clearCountDown = () => {
+  clearInterval(countTime);
+  minutesDecimal = 0;
+  minutesUnity = 0;
+  secoundsDecimal = 0;
+  secoundsUnity = 0;
+  millisecondsDecimal = 0;
+  millisecondsUnity = 0;
+  time.textContent = `${minutesDecimal}${minutesUnity}:${secoundsDecimal}${secoundsUnity}:${millisecondsDecimal}${millisecondsUnity}`;
+};
+
+startButton.addEventListener("click", startCountDown);
+stopButton.addEventListener("click", stopCountDown);
+clearButton.addEventListener("click", clearCountDown);
