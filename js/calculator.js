a66497f5a1923cc5ff81a08a93bcb6f5aae818ed
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-04-17 12:02:13 +0200

    Clear code

--- js/calculator.js
+++ js/calculator.js
@@ -36,11 +36,110 @@ const clearAll = () => {
 const deleteLastSign = () => {
     let sliceCurrentAction = currentAction.slice(0, -1);
     currentAction = sliceCurrentAction;
+    paragraphAction.textContent = currentAction;
 };
+
+const addPercent = () => {
+    if(currentAction === "" || currentAction.endsWith("+") || currentAction.endsWith("-") || currentAction.endsWith("*") || currentAction.endsWith("/") || currentAction.endsWith("(") || currentAction.endsWith(")")) {
+        addNumber("0.1*");
+    } else {
+        addNumber("*0.1");
+    };
+}
+
+const changeTheme = () => {
+    toggleImageMoon.classList.toggle("active");
+    toggleImageSun.classList.toggle("active");
+    body.classList.toggle("lightModeTheme");
+    buttonsNumber.forEach(button => {
+        button.classList.toggle("lightModeButtonNumber");
+    });
+    buttonsFunction.forEach(button => {
+        button.classList.toggle("lightModeButtonFunction");
+    });
+    buttonClear.classList.toggle("lightModeButtonFunction");
+    buttonBrackets.classList.toggle("lightModeButtonFunction");
+    buttonDot.classList.toggle("lightModeButtonDot");
+    buttonTheme.classList.toggle("lightModeButtonFunction");
+    paragraphScore.classList.toggle("lightModeParagraphScore");
+}
+
+buttonAddition.addEventListener("click", () => {
+    if(currentAction === "") {
+        return;
+    };
+    addNumber(buttonAddition.textContent);
+    buttonDot.removeAttribute("disabled", false);
+});
+
+buttonSubtraction.addEventListener("click", () => {
+    if(currentAction === "") {
+        return;
+    };
+    let substraction = "-";
+    addNumber(substraction);
+    buttonDot.removeAttribute("disabled", false);
+});
+
+buttonMultiplication.addEventListener("click", () => {
+    if(currentAction === "") {
+        return;
+    }
+    let multiplication = "*";
+    addNumber(multiplication);
+    buttonDot.removeAttribute("disabled", false);
+});
+
+buttonDivision.addEventListener("click", () => {
+    if(currentAction === "") {
+        return;
+    };
+    addNumber(buttonDivision.textContent);
+    buttonDot.removeAttribute("disabled", false);
+});
+
+buttonPercent.addEventListener("click", () => {
+    addPercent();
+});
+
+buttonDot.addEventListener("click", () => {
+    if(currentAction === "") {
+        addNumber("0.");
+        buttonDot.setAttribute("disabled", true);
+    } else {
+        let dot = ".";
+        addNumber(dot);
+        buttonDot.setAttribute("disabled", true);
+    };
+});
+
+buttonBrackets.addEventListener("click", () => {
+    let bracketsArr = ["(", ")"];
+
+    addNumber(bracketsArr[bracketNumber]);
+    bracketNumber++;
+    if(bracketNumber === 2) {
+        bracketNumber = 0;
+    };
+});
+
+buttonEqual.addEventListener("click", () => {
+    score = eval(currentAction);
+    console.log(score);
+    buttonDot.removeAttribute("disabled", false);
+    paragraphScore.textContent = "= " + score;
+});
+
+buttonClear.addEventListener("click", () => {
+    clearAll();
+});
 
 buttonRemoveLastSign.addEventListener("click", () => {
     deleteLastSign();
-    paragraphAction.textContent = currentAction;
+});
+
+buttonTheme.addEventListener("click", () => {
+    changeTheme()
 });
 
 buttonsNumber.forEach(button => {
@@ -60,95 +159,4 @@ buttonsFunction.forEach(button => {
         buttonMultiplication.setAttribute("disabled", true);
         buttonSubtraction.setAttribute("disabled", true);
     });
-});
-
-buttonPercent.addEventListener("click", () => {
-    if(currentAction === "" || currentAction.endsWith("+") || currentAction.endsWith("-") || currentAction.endsWith("*") || currentAction.endsWith("/") || currentAction.endsWith("(") || currentAction.endsWith(")")) {
-        addNumber("0.1*");
-    } else {
-        addNumber("*0.1");
-    };
-});
-
-buttonAddition.addEventListener("click", () => {
-    if(currentAction === "") {
-        return;
-    };
-    addNumber(buttonAddition.textContent);
-    buttonDot.removeAttribute("disabled", false);
-});
-
-buttonDivision.addEventListener("click", () => {
-    if(currentAction === "") {
-        return;
-    };
-    addNumber(buttonDivision.textContent);
-    buttonDot.removeAttribute("disabled", false);
-});
-
-buttonMultiplication.addEventListener("click", () => {
-    if(currentAction === "") {
-        return;
-    }
-    let multiplication = "*";
-    addNumber(multiplication);
-    buttonDot.removeAttribute("disabled", false);
-});
-
-buttonDot.addEventListener("click", () => {
-    if(currentAction === "") {
-        addNumber("0.");
-        buttonDot.setAttribute("disabled", true);
-    } else {
-        let dot = ".";
-        addNumber(dot);
-        buttonDot.setAttribute("disabled", true);
-    };
-});
-
-buttonSubtraction.addEventListener("click", () => {
-    if(currentAction === "") {
-        return;
-    };
-    let substraction = "-";
-    addNumber(substraction);
-    buttonDot.removeAttribute("disabled", false);
-});
-
-buttonBrackets.addEventListener("click", () => {
-    let bracketsArr = ["(", ")"];
-
-    addNumber(bracketsArr[bracketNumber]);
-    bracketNumber++;
-    if(bracketNumber === 2) {
-        bracketNumber = 0;
-    };
-});
-
-buttonClear.addEventListener("click", () => {
-    clearAll();
-});
-
-buttonEqual.addEventListener("click", () => {
-    score = eval(currentAction);
-    console.log(score);
-    buttonDot.removeAttribute("disabled", false);
-    paragraphScore.textContent = "= " + score;
-});
-
-buttonTheme.addEventListener("click", () => {
-    toggleImageMoon.classList.toggle("active");
-    toggleImageSun.classList.toggle("active");
-    body.classList.toggle("lightModeTheme");
-    buttonsNumber.forEach(button => {
-        button.classList.toggle("lightModeButtonNumber");
-    });
-    buttonsFunction.forEach(button => {
-        button.classList.toggle("lightModeButtonFunction");
-    });
-    buttonClear.classList.toggle("lightModeButtonFunction");
-    buttonBrackets.classList.toggle("lightModeButtonFunction");
-    buttonDot.classList.toggle("lightModeButtonDot");
-    buttonTheme.classList.toggle("lightModeButtonFunction");
-    paragraphScore.classList.toggle("lightModeParagraphScore");
 });
